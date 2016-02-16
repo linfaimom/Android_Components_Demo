f6574cda95f87f46ff9060c66128194c0fc24ca7
marcus <linfaimom@gmail.com>
2016-02-16 23:31:08 +0800

    add a new project

--- IntentServiceTest/app/src/main/java/com/example/marcus/intentservicetest/MainActivity.java
+++ IntentServiceTest/app/src/main/java/com/example/marcus/intentservicetest/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.marcus.intentservicetest;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
 import android.view.View;
 import android.widget.Button;
 
--- TelephonyManager/app/src/main/java/com/example/marcus/telephonymanager/MainActivity.java
+++ TelephonyManager/app/src/main/java/com/example/marcus/telephonymanager/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.marcus.telephonymanager;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.ListView;
