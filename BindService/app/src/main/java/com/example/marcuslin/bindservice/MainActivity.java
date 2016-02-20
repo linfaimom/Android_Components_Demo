734282ed16fd6e90de1a2b5c14379d1e6864f0f6
GodXiang <linfaimom@gmail.com>
2016-02-20 12:25:44 +0800

    fix merge

--- BindService/app/src/main/java/com/example/marcuslin/bindservice/MainActivity.java
+++ BindService/app/src/main/java/com/example/marcuslin/bindservice/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
